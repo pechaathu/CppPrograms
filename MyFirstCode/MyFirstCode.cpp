bb92f98be7adf195f9b07d76524e26606b5b11c1
Eswara Prasath M <muruganeswaraprasath@gmail.com>
2021-09-30 18:08:57 +0530

    upto My First Code

---
+++ MyFirstCode/MyFirstCode.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "This is gonna be Okay..."<<endl;
+    cout << "You will get placed in the best company...";
+    system("pause>0");
+}
