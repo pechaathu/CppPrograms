c58afe1d720512055d3f1bb0a0944129ac220af1
Eswara Prasath M <muruganeswaraprasath@gmail.com>
2021-10-01 11:18:00 +0530

    upto Salary Code

---
+++ SalaryCode/SalaryCode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float monthlySalary;
+    cout << "Enter your monthly salary: ";
+    cin >> monthlySalary;
+    cout << "Your annual salary is : " << monthlySalary * 12;
+    system("pause>0");
+}
